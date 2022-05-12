57345f7ce6828aba402d47dcfb90d3834c4578ac
duongtm312 <103014462+duongtm312@users.noreply.github.com>
2022-05-12 16:17:46 +0700

    up

---
+++ src/Student.java
@@ -0,0 +1,13 @@
+public class Student {
+    private String name = "Jhon";
+    private String clas = "C02";
+    Student(){}
+
+    public void setClas(String clas) {
+        this.clas = clas;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/Test.java
@@ -0,0 +1,7 @@
+public class Test {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setClas("C03");
+        student.setName("D");
+    }
+}
